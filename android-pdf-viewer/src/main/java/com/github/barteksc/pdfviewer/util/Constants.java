67b3e4dcf41e4ac6a0c082fee55686f2fa33647c
Cansu Köse <cansu.kose@keove.com>
2025-07-02 16:02:43 +0300

    change value

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/util/Constants.java
@@ -20,14 +20,14 @@ public class Constants {
     public static boolean DEBUG_MODE = false;
 
     /** Between 0 and 1, the thumbnails quality (default 0.3). Increasing this value may cause performance decrease */
-    public static float THUMBNAIL_RATIO = 0.1f;
+    public static float THUMBNAIL_RATIO = 0.4f;
 
     /**
      * The size of the rendered parts (default 256)
      * Tinier : a little bit slower to have the whole page rendered but more reactive.
      * Bigger : user will have to wait longer to have the first visual results
      */
-    public static float PART_SIZE = 900;
+    public static float PART_SIZE = 700;
 
     /** Part of document above and below screen that should be preloaded, in dp */
     public static int PRELOAD_OFFSET = 10;
@@ -35,9 +35,9 @@ public class Constants {
     public static class Cache {
 
         /** The size of the cache (number of bitmaps kept) */
-        public static int CACHE_SIZE = 100;
+        public static int CACHE_SIZE = 150;
 
-        public static int THUMBNAILS_CACHE_SIZE = 8;
+        public static int THUMBNAILS_CACHE_SIZE = 6;
     }
 
     public static class Pinch {
